af0995aa3d3256f77dc899b6d1c7a6b112966828
Lazaros Toumanidis <laztoum@protonmail.com>
2024-12-23 01:42:30 +0200

    ci, add ts tests

--- scripts/_ts/test.ts
+++ scripts/_ts/test.ts
@@ -1,3 +1,21 @@
 /** Run tests in the project's subdirectories. */
+import path from "path";
+import fs from "fs-extra";
 
-console.log("Running tests ...");
+import { packageJson, rootDir, runCommandInDir, getPackageManager } from "../_ts/_lib";
+
+
+function runTests() {
+    for (const project of packageJson.packages.ts) {
+        const projectDir = path.join(rootDir, project);
+        const packageJsonPath = path.join(projectDir, "package.json");
+        if (fs.existsSync(packageJsonPath)) {
+            const packageManager = getPackageManager(projectDir);
+            runCommandInDir(projectDir, packageManager, ["run", "test"]);
+        } else {
+            console.log(`Skipping ${projectDir} as it does not have a package.json file.`);
+        }
+    }
+}
+
+runTests();
