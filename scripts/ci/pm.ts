6dcd1514e28ee51c73d26c54095067097f0d0878
Lazaros Toumanidis <laztoum@protonmail.com>
2025-03-11 18:16:55 +0200

    update deps, sync submodules

--- scripts/ci/pm.ts
+++ scripts/ci/pm.ts
@@ -5,8 +5,8 @@ let's patch (and un-patch) the package manager in package.json
 import fs from "fs-extra";
 import packageJson from "../../package.json";
 
-const yarn = "yarn@4.6.0";
-const bun = "bun@1.2.2";
+const yarn = "yarn@4.7.0";
+const bun = "bun@1.2.5";
 
 // check arg (if --patch or --rollback)
 
