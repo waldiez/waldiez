93982a542d1c8cc67f6106200542700dc43573dd
Lazaros Toumanidis <laztoum@waldiez.io>
2025-06-01 22:46:46 +0300

    update front

--- src/waldiez/models/mappers/agent/agentMapper.ts
+++ src/waldiez/models/mappers/agent/agentMapper.ts
@@ -130,6 +130,7 @@ export const agentMapper = {
             "createdAt",
             "updatedAt",
             "agentType",
+            "parentId",
         ]) {
             delete agentCopy[key];
         }
