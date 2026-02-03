d27635ec2f4dd89516efdd60b235f6788908bbb2
Lazaros Toumanidis <laztoum@protonmail.com>
2026-02-03 15:00:41 +0200

    lint

---
+++ docs/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0.
+# Copyright (c) 2024 - 2026 Waldiez and contributors.
+"""Dummy __init__.py"""
+
+# To avoid:
+# error: Duplicate module named "waldiez" (also at "./docs/macros/waldiez.py")
